b1f51eac8573fa1e7e6cd21d2c50a3aefc73f844
Huw Jones <huwcbjones@gmail.com>
2016-01-21 22:48:53 +0000

    Added config.lua and defines.lua

---
+++ lua/config.lua
@@ -0,0 +1,29 @@
+--
+-- Config for Loadout Manager
+--
+-- Author: Huw Jones
+-- Date:   21/01/2016
+--
+
+-- Filter for primary weapons (SLOT_3)
+
+-- Specify filter type. Available types:
+-- LoadoutMgr.FILTER_NONE
+-- LoadoutMgr.FILTER_BLACKLIST
+-- LoadoutMgr.FILTER_WHITELIST
+LoadoutMgr.Weapons.Filter.Primary.Type = LoadoutMgr.FILTER_NONE
+
+-- Use the weapon name (e.g.: ttt_weapon_knife).
+LoadoutMgr.Weapons.Filter.Primary.Value = ""
+
+-- Filter for secondary weapons (SLOT_2)
+
+-- Specify filter type. Available types:
+-- LoadoutMgr.FILTER_NONE
+-- LoadoutMgr.FILTER_BLACKLIST
+-- LoadoutMgr.FILTER_WHITELIST
+LoadoutMgr.Weapons.Filter.Secondary.Type = LoadoutMgr.FILTER_NONE
+
+-- Use the weapon name (e.g.: ttt_weapon_knife).
+LoadoutMgr.Weapons.Filter.Secondary.Value = ""
+
---
+++ lua/defines.lua
@@ -0,0 +1,22 @@
+--
+-- Constant Definitions
+--
+-- Author: Huw Jones
+-- Date:   21/01/2016
+--
+
+-- Filter options
+LoadoutMgr.FILTER_NONE = 0
+LoadoutMgr.FILTER_BLACKLIST = 1
+LoadoutMgr.FILTER_WHITELIST = 2
+
+LoadoutMgr.WEAPON_PRIMARY = 3
+LoadoutMgr.WEAPON_SECONDARY = 2
+
+LoadoutMgr.validCommands = {
+    ["primary"] = "primary",
+    ["secondary"] = "secondary",
+    ["override"] = "override",
+    ["reset"] = "reset",
+    ["help"] = "help"
+}
