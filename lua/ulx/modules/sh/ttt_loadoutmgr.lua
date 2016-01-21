d4d7d182500c05c9a4f69aa3f4cd3937f7554db9
Huw Jones <huwcbjones@gmail.com>
2016-01-21 22:49:12 +0000

    Added ulx command module for loadout manager

---
+++ lua/ulx/modules/sh/ttt_loadoutmgr.lua
@@ -0,0 +1,15 @@
+--
+-- Adds ULX Command integration to Loadout Manager
+--
+-- Author: Huw Jones
+-- Date:   21/01/2016
+--
+
+local CATAGORY_NAME = "Loadout"
+local gamemode_error = "TTT is required to use this addon!"
+
+local loadout = ulx.command(CATAGORY_NAME, "ulx loadout", LoadoutMgr.manager, "!loadout", true)
+loadout:defaultAccess( ULib.ACCESS_ADMIN )
+loadout:addParam{ type=ULib.cmds.StringArg , hint="Command", ULib.cmds.optional }
+loadout:addParam{ type=ULib.cmds.StringArg , hint="Argument", ULib.cmds.optional}
+loadout:help("Manages the player's loadout.")
