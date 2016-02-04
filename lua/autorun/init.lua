a331d405e0e2e9fa422d9a93e4dad50b8f0c8afa
Huw Jones <huwbjones@gmail.com>
2016-02-04 14:25:53 +0000

    Added weapon name map load

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -10,6 +10,7 @@
 --
 local function loadDefaultConfig()
     LoadoutMgr.Weapons = {}
+    LoadoutMgr.Weapons.NameMap = {}
     LoadoutMgr.Weapons.Primary = {}
     LoadoutMgr.Weapons.Secondary = {}
 
@@ -29,6 +30,17 @@ end
 local function loadConfig()
     if not file.Exists("ttt_loadout_manager/config.lua", "LUA") then return end
     include("ttt_loadout_manager/config.lua")
+end
+
+local function loadWeaponNameMap()
+    if (not file.Exists("data/weapon_map.json")) then
+        Msg("[LOADOUT MGR] ERROR! Weapon name map not found!")
+    end
+    local map = file.Open("weapon_map.json", "r", "DATA")
+    local table = util.JSONToTable(map)
+    if table ~= nil then
+        LoadoutMgr.Weapons.NameMap = table
+    end
 end
 
 if not LoadoutMgr then
