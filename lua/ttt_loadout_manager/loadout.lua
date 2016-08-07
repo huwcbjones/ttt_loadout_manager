ebc30e219902d5efe47a80b85e81b5bc5ec0639e
Huw Jones <huwcbjones@gmail.com>
2016-08-07 16:13:51 +0100

    Fixed override setting. Fixed UCL. Commented.

--- lua/ttt_loadout_manager/loadout.lua
+++ lua/ttt_loadout_manager/loadout.lua
@@ -5,7 +5,16 @@
 -- Date:   21/01/2016
 --
 
+--- Loadout manager main function
+-- @param calling_player Player
+-- @param arg2 Arg2
+-- @param arg3 Arg3
+--
 LoadoutMgr.manager = function(calling_player, arg2, arg3)
+    if (not calling_player:query("ulx loadout", true)) then
+        ULib.tsayError(calling_player, "You don't have permission to use the Loadout Manager!")
+        return
+    end
     -- If no arguements specified, display loadout
     if ((arg2 == nil or arg2 == "") and (arg3 == nil or arg3 == "")) then
         LoadoutMgr.printLoadout(calling_player)
@@ -72,11 +81,18 @@ LoadoutMgr.manager = function(calling_player, arg2, arg3)
     -- Set weapon override status
     if (arg2 == "override") then
         if (not LoadoutMgr.checkOverride(arg3)) then
-            ULib.tsayError(calling_player, "Invalid override weapon status \"" .. arg3 .. "\"")
+            ULib.tsayError(calling_player, "Invalid override setting \"" .. arg3 .. "\"")
             return
         end
-        if arg3 == "" then arg3 = nil end
+        ULib.tsayColor(calling_player, false, Color(255, 0, 0), "== Loadout Manager ==")
         LoadoutMgr.setWeaponOverride(calling_player, arg3)
+
+        local override_str = "Disabled"
+        if (LoadoutMgr.getWeaponOverride(calling_player)) then
+            override_str = "Enabled"
+        end
+
+        ULib.tsayColor(calling_player, false, Color(0, 128, 255), "Override Weapons: " .. override_str)
     end
 
     -- Display weapons
@@ -85,11 +101,15 @@ LoadoutMgr.manager = function(calling_player, arg2, arg3)
     end
 end
 
+--- Displays the loadout help
+-- @param calling_player Player
+-- @param command Command for help
+--
 LoadoutMgr.displayHelp = function(calling_player, command)
     ULib.tsayColor(calling_player, false, Color(255, 0, 0), "Loadout Manager Help: ", Color(255, 192, 0), command)
 
     if (command == "primary" or command == "secondary") then
-        ULib.tsayColor(calling_player, false, "!loadout [primary/secondary] [weapon]")
+        ULib.tsayColor(calling_player, false, "!loadout " .. command .. " [weapon]")
         ULib.tsayColor(calling_player, false, "If weapon is blank, your loadout slot for that weapon will be cleared.")
         return
     end
@@ -100,7 +120,7 @@ LoadoutMgr.displayHelp = function(calling_player, command)
     end
 
     if (command == "override") then
-        ULib.tsayColor(calling_player, false, "!loadout override [true/false], sets whether your current weapons will be replaced when your loadout is equipped")
+        ULib.tsayColor(calling_player, false, "!loadout override [on/off], sets whether your current weapons will be replaced when your loadout is equipped")
         return
     end
 
@@ -122,6 +142,9 @@ LoadoutMgr.displayHelp = function(calling_player, command)
     ULib.tsayColor(calling_player, false, Color(0, 192, 255), "Available commands: " .. cmds)
 end
 
+--- Prints a player's loadout
+-- @param calling_player Player
+--
 LoadoutMgr.printLoadout = function(calling_player)
     local primary = LoadoutMgr.getWeaponPrimary(calling_player)
     local secondary = LoadoutMgr.getWeaponSecondary(calling_player)
@@ -143,6 +166,10 @@ LoadoutMgr.printLoadout = function(calling_player)
     ULib.tsayColor(calling_player, false, Color(0, 128, 255), "Override: " .. override_str)
 end
 
+--- Prints the list of weapons
+-- @param calling_player
+-- @param type
+--
 LoadoutMgr.printWeapons = function(calling_player, type)
     LoadoutMgr.getWeaponList()
 
@@ -166,6 +193,8 @@ LoadoutMgr.printWeapons = function(calling_player, type)
     ULib.tsayError(calling_player, "Please specify a weapon type (primary/secondary)")
 end
 
+--- Gets the list of weapons available
+--
 LoadoutMgr.getWeaponList = function()
     local weaponList = weapons.GetList()
     local weapon
@@ -183,17 +212,17 @@ LoadoutMgr.getWeaponList = function()
     end
 end
 
--- Checks is a primary weapon is allowed
+--- Checks is a primary weapon is allowed
 --
--- @param weapon    String, weapon ID string
+-- @param weapon String, weapon ID string
 -- @return boolean  True if the weapon is allowed, false if it is not
 LoadoutMgr.checkPrimaryWeapon = function(weapon)
     return LoadoutMgr.Weapons.Primary[weapon] ~= nil
 end
 
--- Checks the Primary weapon if it passes the filter
+--- Checks the Primary weapon if it passes the filter
 --
--- @param weapon    String, weapon ID string
+-- @param weapon String, weapon ID string
 -- @return boolean  True if the weapon is allowed, false if it is not
 LoadoutMgr.checkPrimaryWeaponFilter = function(weapon)
     --TODO: Add weapon filtering
@@ -201,18 +230,18 @@ LoadoutMgr.checkPrimaryWeaponFilter = function(weapon)
     return true
 end
 
--- Checks is a secondary weapon is allowed
+--- Checks is a secondary weapon is allowed
 --
--- @param weapon    String, weapon ID string
+-- @param weapon String, weapon ID string
 -- @return boolean  True if the weapon is allowed, false if it is not
 LoadoutMgr.checkSecondaryWeapon = function(weapon)
     return LoadoutMgr.Weapons.Secondary[weapon] ~= nil
 end
 
 
--- Checks the Secondary weapon if it passes the filter
+--- Checks the Secondary weapon if it passes the filter
 --
--- @param weapon    String, weapon ID string
+-- @param weapon String, weapon ID string
 -- @return boolean  True if the weapon is allowed, false if it is not
 LoadoutMgr.checkSecondaryWeaponFilter = function(weapon)
     --TODO: Add weapon filtering
@@ -220,10 +249,16 @@ LoadoutMgr.checkSecondaryWeaponFilter = function(weapon)
     return true
 end
 
+--- Checks whether the override param meets the criteria
+-- @param state On/off
+--
 LoadoutMgr.checkOverride = function(state)
-    return (state ~= "true" or state ~= false or state ~= "")
+    return (state == "on" or state == "off")
 end
 
+--- Converts a weapon className to the name string
+-- @param weapon Weapon className
+--
 LoadoutMgr.convertWeaponToString = function(weapon)
     local SWEP = weapons.Get(weapon)
     if SWEP == nil then
@@ -235,31 +270,64 @@ LoadoutMgr.convertWeaponToString = function(weapon)
     return LoadoutMgr.Weapons.NameMap[weapon]
 end
 
+--- Gets the player's primary weapon
+-- @param player Player
+--
 LoadoutMgr.getWeaponPrimary = function(player)
     return player:GetPData("loadoutMgr_weapon_primary", nil)
 end
 
+--- Sets the player's primary weapon
+-- @param player Player
+-- @param weapon Weapon ClassName
+--
 LoadoutMgr.setWeaponPrimary = function(player, weapon)
     player:SetPData("loadoutMgr_weapon_primary", weapon)
 end
 
+--- Gets the player's secondary weapon
+-- @param player Player
+--
 LoadoutMgr.getWeaponSecondary = function(player)
     return player:GetPData("loadoutMgr_weapon_secondary", nil)
 end
 
+--- Sets the player's secopndary weapon
+-- @param player Player
+-- @param weapon Weapon ClassName
+--
 LoadoutMgr.setWeaponSecondary = function(player, weapon)
     player:SetPData("loadoutMgr_weapon_secondary", weapon)
 end
 
+--- Gets whether or not the player's currently equipped weapons will be overriden on loadout loading
+-- @param player Player
+--
 LoadoutMgr.getWeaponOverride = function(player)
-    return player:GetPData("loadoutMgr_weapon_override", false)
+    local override = player:GetPData("loadoutMgr_weapon_override", false)
+    if (override == "off" or override == nil) then
+        return false
+    else
+        return true
+    end
 end
 
+--- Gets whether or not the player's currently equipped weapons will be overriden on loadout loading
+-- @param player Player
+-- @param state true/false
+--
 LoadoutMgr.setWeaponOverride = function(player, state)
     player:SetPData("loadoutMgr_weapon_override", state)
 end
 
 if SERVER then
+
+    --- Manages the equipping of a players loadout
+    -- @param player Player
+    -- @param type Slot to equip
+    -- @param shouldOverride Whether or not the current weapon in slot `type` should be override
+    -- @param loadoutWeapon Weapon ClassName to equip
+    --
     local function processEquipWeapon(player, type, shouldOverride, loadoutWeapon)
         local shouldEquip = true
         local playerWeapons = player:GetWeapons()
@@ -288,9 +356,11 @@ if SERVER then
         end
     end
 
+    --- Server hook for managing loadouts on round start
+    --
     local function onRoundStart()
         for _, player in pairs(player.GetAll()) do
-            if (not player:query( "ulx loadout")) then
+            if (not player:query("ulx loadout", true)) then
                 return
             end
             ULib.tsayColor(player, false, Color(255, 0, 0), "[LOADOUT MGR] Loading your loadout...")
@@ -318,6 +388,8 @@ if SERVER then
         end
     end
 
+    --- Refresh weapon list on preparing
+    --
     local function onPreparing()
         LoadoutMgr.getWeaponList()
     end
